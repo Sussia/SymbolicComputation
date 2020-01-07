322963bd9203b7beb2a8a66196222e40d37d8086
ArtamonovAlex <artamonovalex18@gmail.com>
2020-01-07 20:24:20 +0300

    Added List func and func to define varaibles

--- SymbolicComputation/BuildInFunctions.cs
+++ SymbolicComputation/BuildInFunctions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using SymbolicComputation.Model;
@@ -10,6 +11,7 @@ namespace SymbolicComputation
 {
 	public static class BuildInFunctions
 	{
+		private static Scope context = new Scope();
 
 		private static Dictionary<string, Func<Expression, Symbol>> functionsDictionary =
 			new Dictionary<string, Func<Expression, Symbol>>
@@ -19,10 +21,11 @@ namespace SymbolicComputation
 				{"Mul", Mul},
 				{"Div", Div},
 				{"Rem", Rem},
-				{"Divide", Divide}
+				{"Divide", Divide},
+				{"List", List}
 			};
 
-		private static Symbol Eval(Expression exp, Func<decimal, decimal, decimal> func)
+		private static Symbol MathEval(Expression exp, Func<decimal, decimal, decimal> func)
 		{
 			var arg1 = exp.Args[0];
 			var arg2 = exp.Args[1];
@@ -37,6 +40,10 @@ namespace SymbolicComputation
 
 		public static Symbol Evaluate(Expression exp)
 		{
+			if (exp.Action.ToString() == "Set")
+			{
+				return Set(exp);
+			}
 			List<Symbol> newArgs = new List<Symbol>();
 			foreach (var arg in exp.Args)
 			{
@@ -44,7 +51,10 @@ namespace SymbolicComputation
 				{
 					newArgs.Add(Evaluate((Expression)arg));
 				}
-				else
+				else if (arg is StringSymbol symbol)
+				{
+					newArgs.Add(Substitute(symbol));
+				} else
 				{
 					newArgs.Add(arg);
 				}
@@ -56,32 +66,66 @@ namespace SymbolicComputation
 
 		private static Symbol Sum(Expression exp)
 		{
-			return Eval(exp, (a, b) => a + b);
+			return MathEval(exp, (a, b) => a + b);
 		}
 
 		private static Symbol Sub(Expression exp)
 		{
-			return Eval(exp, (a, b) => a - b);
+			return MathEval(exp, (a, b) => a - b);
 		}
 
 		private static Symbol Mul(Expression exp)
 		{
-			return Eval(exp, (a, b) => a * b);
+			return MathEval(exp, (a, b) => a * b);
 		}
 
 		private static Symbol Divide(Expression exp)
 		{
-			return Eval(exp, (a, b) => a / b);
+			return MathEval(exp, (a, b) => a / b);
 		}
 
 		private static Symbol Div(Expression exp)
 		{
-			return Eval(exp, (a, b) => (int)(a / b));
+			return MathEval(exp, (a, b) => (int)(a / b));
 		}
 
 		private static Symbol Rem(Expression exp)
 		{
-			return Eval(exp, (a, b) => a % b);
+			return MathEval(exp, (a, b) => a % b);
+		}
+
+		private static Symbol List(Expression exp)
+		{
+			return exp.Args.Last();
+		}
+
+		private static Symbol Set(Expression exp)
+		{
+			var arg1 = exp.Args[0];
+			var arg2 = exp.Args[1];
+			if (arg1 is StringSymbol symbol)
+			{
+				Symbol newArg;
+				if (arg2 is Expression exp2)
+				{
+					newArg = Evaluate(exp2);
+				} else if (arg2 is StringSymbol stringSymbol)
+				{
+					newArg = Substitute(stringSymbol);
+				}
+				else
+				{
+					newArg = arg2;
+				}
+				context.SymbolRules.Add(symbol.Name, newArg);
+				return arg1;
+			}
+			throw new Exception("First parameter of Set isn't string symbol");
+		}
+
+		private static Symbol Substitute(StringSymbol symbol)
+		{
+			return context.SymbolRules[symbol.Name]; //, out var resultSymbol) ? resultSymbol : symbol;
 		}
 	}
 }
--- SymbolicComputation/Program.cs
+++ SymbolicComputation/Program.cs
@@ -9,15 +9,27 @@ namespace SymbolicComputation
 		private static void Main(string[] args)
 		{
 			Symbol Sum = new StringSymbol("Sum");
-			Expression exp1 = Sum[5,3];
+			Expression exp1 = Sum[5, 3];
 
 			Symbol Mul = new StringSymbol("Mul");
 
 			Expression exp2 = Mul[5, exp1];
 
-			Symbol res = exp2.Evaluate();
+			Expression exp3 = Mul["x", "y"];
+
+			Symbol Set = new StringSymbol("Set");
+
+			Expression setExp = Set["x", 2];
+			Expression setExp2 = Set["y", 3];
+
+			Symbol List = new StringSymbol("List");
+
+			Expression exp4 = List[setExp, setExp2, exp3];
+
+			Symbol res = exp4.Evaluate();
 
 			Console.WriteLine(res.ToString());
+
 		}
 	}
 }

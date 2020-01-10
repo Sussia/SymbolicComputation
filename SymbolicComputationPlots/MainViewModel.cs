521b6aed06c5810680eb5b26fe3c5b9899711250
ArtamonovAlex <artamonovalex18@gmail.com>
2020-01-10 20:35:51 +0300

    Add blank for plot drawing

---
+++ SymbolicComputationPlots/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace SymbolicComputationPlots
+{
+	public class MainViewModel
+	{
+		public MainViewModel()
+		{
+			this.MyModel = new PlotModel { Title = "Example 1" };
+			this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+		}
+
+		public PlotModel MyModel { get; private set; }
+	}
+}
